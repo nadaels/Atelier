e8790a3f41e2a9146cda672accf539b0a2a4d3ae
Nada ELS <nada.elsemary@outlook.com>
2026-05-20 18:39:37 +0200

    Ajout des tests Jest pour AddForm

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('vérifie que l\'application se charge', () => {
-  render(<App />);
-  expect(true).toBe(true);
-});
